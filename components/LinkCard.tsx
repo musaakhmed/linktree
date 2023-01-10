1f58235d5a3a4d9a3ce92049df410a73afba5bc8
Musa Akhmedov <musa.punhan@pm.me>
2023-01-10 21:02:57 +0100

    social icons scale on hover

--- components/LinkCard.tsx
+++ components/LinkCard.tsx
@@ -25,7 +25,7 @@ const LinkCard = ({
 
                 <span className='text-sm'>{title}</span>
 
-                <div className=' group-hover:opacity-100 p-2 opacity-0 hover:opacity-100 transition-all duration-200 ease-in-out'>
+                <div className=' group-hover:opacity-100 p-2 opacity-0 hover:opacity-100 transition-all duration-500 ease-in-out'>
                     <Image
                         src='/share.png'
                         width={24}
--- components/Socials.tsx
+++ components/Socials.tsx
@@ -13,28 +13,44 @@ const Socials = () => {
             {data.socials.map((social) => {
                 if (social.url.includes('twitter')) {
                     return (
-                        <a key={social.url} href={social.url}>
+                        <a
+                            key={social.url}
+                            href={social.url}
+                            className='hover:scale-110 duration-200 ease-in-out'
+                        >
                             <TwitterIcon />
                         </a>
                     )
                 }
                 if (social.url.includes('linkedin')) {
                     return (
-                        <a key={social.url} href={social.url}>
+                        <a
+                            key={social.url}
+                            href={social.url}
+                            className='hover:scale-110 duration-200 ease-in-out'
+                        >
                             <LinkedinIcon />
                         </a>
                     )
                 }
                 if (social.url.includes('instagram')) {
                     return (
-                        <a key={social.url} href={social.url}>
+                        <a
+                            key={social.url}
+                            href={social.url}
+                            className='hover:scale-110 duration-200 ease-in-out'
+                        >
                             <InstagramIcon />
                         </a>
                     )
                 }
                 if (social.url.includes('github')) {
                     return (
-                        <a key={social.url} href={social.url}>
+                        <a
+                            key={social.url}
+                            href={social.url}
+                            className='hover:scale-110 duration-200 ease-in-out'
+                        >
                             <GitHubIcon />
                         </a>
                     )
